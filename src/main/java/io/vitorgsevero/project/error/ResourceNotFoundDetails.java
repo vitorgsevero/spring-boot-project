a8a6851cfc80916269b46e2b407a2de78540eedb
vitorgsevero <vitorgsevero@gmail.com>
2019-10-24 16:53:25 -0300

    Handling Errors: Field Validation

--- src/main/java/io/vitorgsevero/project/error/ResourceNotFoundDetails.java
+++ src/main/java/io/vitorgsevero/project/error/ResourceNotFoundDetails.java
@@ -1,35 +1,6 @@
 package io.vitorgsevero.project.error;
 
-public class ResourceNotFoundDetails {
-
-    private String title;
-    private int status;
-    private String detail;
-    private long timestamp;
-    private String developerMessage;
-
-    private ResourceNotFoundDetails() {
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public String getDetail() {
-        return detail;
-    }
-
-    public long getTimestamp() {
-        return timestamp;
-    }
-
-    public String getDeveloperMessage() {
-        return developerMessage;
-    }
+public class ResourceNotFoundDetails extends ErrorDetail{
 
     public static final class Builder {
         private String title;
@@ -72,11 +43,11 @@ public class ResourceNotFoundDetails {
 
         public ResourceNotFoundDetails build() {
             ResourceNotFoundDetails resourceNotFoundDetails = new ResourceNotFoundDetails();
-            resourceNotFoundDetails.timestamp = this.timestamp;
-            resourceNotFoundDetails.status = this.status;
-            resourceNotFoundDetails.detail = this.detail;
-            resourceNotFoundDetails.title = this.title;
-            resourceNotFoundDetails.developerMessage = this.developerMessage;
+            resourceNotFoundDetails.setTimestamp(timestamp);
+            resourceNotFoundDetails.setStatus(status);
+            resourceNotFoundDetails.setDetail(detail);
+            resourceNotFoundDetails.setTitle(title);
+            resourceNotFoundDetails.setDeveloperMessage(developerMessage);
             return resourceNotFoundDetails;
         }
     }
--- src/main/java/io/vitorgsevero/project/handler/RestExceptionHandler.java
+++ src/main/java/io/vitorgsevero/project/handler/RestExceptionHandler.java
@@ -2,8 +2,10 @@ package io.vitorgsevero.project.handler;
 
 import io.vitorgsevero.project.error.ResourceNotFoundDetails;
 import io.vitorgsevero.project.error.ResourceNotFoundException;
+import io.vitorgsevero.project.error.ValidationErrorDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,6 +22,20 @@ public class RestExceptionHandler {
                             .title("Resource not found")
                             .detail(rnfException.getMessage())
                             .developerMessage(rnfException.getClass().getName())
+                            .build();
+                return new ResponseEntity<>(rfnDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException manvException){
+        System.out.println(manvException.getBindingResult().getFieldError());
+        ValidationErrorDetails rfnDetails = ValidationErrorDetails.Builder
+                            .newBuilder()
+                            .timestamp(new Date().getTime())
+                            .status(HttpStatus.NOT_FOUND.value())
+                            .title("Resource not found")
+                            .detail(manvException.getMessage())
+                            .developerMessage(manvException.getClass().getName())
                             .build();
                 return new ResponseEntity<>(rfnDetails, HttpStatus.NOT_FOUND);
     }
