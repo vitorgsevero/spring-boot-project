776e91b4ea092ae63c2737a4d9ebbd6dc8664ffc
vitorgsevero <vitorgsevero@gmail.com>
2019-10-28 17:48:09 -0300

    Authentication and Authorization with Spring Data

--- src/main/java/io/vitorgsevero/project/adapter/SpringBootEssentialsAdapter.java
+++ src/main/java/io/vitorgsevero/project/adapter/SpringBootEssentialsAdapter.java
@@ -11,7 +11,6 @@ import java.util.List;
 @Configuration
 public class SpringBootEssentialsAdapter implements WebMvcConfigurer {
 
-
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         PageableHandlerMethodArgumentResolver phmar = new PageableHandlerMethodArgumentResolver();
--- src/main/java/io/vitorgsevero/project/config/SecurityConfig.java
+++ src/main/java/io/vitorgsevero/project/config/SecurityConfig.java
@@ -1,15 +1,21 @@
 package io.vitorgsevero.project.config;
 
+import io.vitorgsevero.project.service.CustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -20,11 +26,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable();
     }
 
-    @Autowired
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    /*    @Autowired
     public void configureGlobal (AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("vitorgsevero").password("{noop}vitor123").roles("USER")
                 .and()
                 .withUser("admin").password("{noop}admin").roles("USER", "ADMIN");
-    }
+    }*/
 }
